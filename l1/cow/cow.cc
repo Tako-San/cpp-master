7fa8f76f4150ef52868c603cc99482b362bcf7b1
Tako-San <dxe13@mail.ru>
2024-09-21 17:51:51 +0300

    Some stuff

--- l1/cow/cow.cc
+++ l1/cow/cow.cc
@@ -1,6 +1,13 @@
-#include <iostream>
 #include <memory>
 #include <string>
+#include <string_view>
+
+#include <range/v3/all.hpp>
+
+#include <fmt/ranges.h>
+
+namespace rng = ranges;
+namespace vws = rng::views;
 
 template <typename CharT, typename Traits = std::char_traits<CharT>,
           typename Alloc = std::allocator<CharT>>
@@ -8,29 +15,70 @@ class BasicCOWString final {
 private:
   using COWStringT = BasicCOWString<CharT, Traits, Alloc>;
   using StringT = std::basic_string<CharT, Traits, Alloc>;
+  using StringViewT = std::basic_string_view<CharT, Traits>;
 
-  std::shared_ptr<StringT> str_;
-
-  class Proxy final {
-    // TODO: think
-  };
+  std::shared_ptr<const StringT> str_;
 
 public:
   template <typename... Args>
   BasicCOWString(Args &&...args)
-      : str_(std::make_shared<StringT>(std::forward<Args>(args)...)) {}
+      : str_(std::make_shared<const StringT>(std::forward<Args>(args)...)) {}
   BasicCOWString(const COWStringT &str) = default;
   BasicCOWString(COWStringT &&str) = default;
 
   COWStringT &operator=(const COWStringT &str) = default;
   COWStringT &operator=(COWStringT &&str) = default;
 
-  bool empty() const { return str_->empty(); }
+  auto empty() const { return str_->empty(); }
+  auto size() const { return str_->size(); }
+  auto data() const { return StringViewT{*str_}; }
+  auto find(StringViewT s) const { return str_->find(s); }
+};
+
+template <typename CharT, typename Traits = std::char_traits<CharT>,
+          typename Alloc = std::allocator<CharT>>
+class COWTokenizer final {
+public:
+  using COWStringT = BasicCOWString<CharT, Traits, Alloc>;
+  using StringViewT = std::basic_string_view<CharT, Traits>;
+
+private:
+  CharT delim_;
+  COWStringT str_;
+  std::size_t begin_;
+
+public:
+  COWTokenizer(const COWStringT &str, CharT delim)
+      : delim_(delim), str_(str), begin_(0) {}
+
+  auto get() {
+    auto str = str_.data();
+    auto begin = str.find_first_not_of(delim_, begin_);
+    if (begin == str.npos)
+      return StringViewT{};
+
+    auto last = str.find_first_of(delim_, begin);
+    if (last != str.npos)
+      str.remove_suffix(str.size() - last);
+
+    str.remove_prefix(begin);
+    begin_ = last;
+    return str;
+  }
 };
 
 using COWString = BasicCOWString<char>;
 
 int main() {
-  COWString str{"Hi"};
-  std::cout << str.empty() << std::endl;
+  COWString str{"  Hello  world!  My name   is     Dio!"};
+  auto tokenizer = COWTokenizer{str, ' '};
+  auto tokens = tokenizer.get();
+  while (!tokens.empty()) {
+    fmt::println("{}", tokens);
+    tokens = tokenizer.get();
+  }
+
+  fmt::println("{}", str.data());
+  fmt::println("{}", str.find("world"));
+  return 0;
 }
---
+++ l1/cow/cow.hh
@@ -0,0 +1,5 @@
+#ifndef __L1_COW_COW_HH__
+#define __L1_COW_COW_HH__
+
+
+#endif // __L1_COW_COW_HH__
